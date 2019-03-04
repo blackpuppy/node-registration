11ecd8c093bc9cd7768ebffcb6b52ee68747153e
Zhu Ming <mingzhu.z+github@gmail.com>
2019-03-05 07:39:34 +0800

    database migration

--- migrations/20190304000030_create_registrations_table.js
+++ migrations/20190304000030_create_registrations_table.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
     t.increments('id').primary();
     t.integer('teacher_id').unsigned().notNullable();
     t.integer('student_id').unsigned().notNullable();
-    t.boolean('suspended').notNullable();
+    t.boolean('suspended').notNullable().defaultTo(0);
     t.timestamps(false, true);
 
     t.unique(['teacher_id', 'student_id']);
